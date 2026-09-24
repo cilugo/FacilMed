b71e2fcb11fd771159212ab0fcb8165d1350e150
mariilps <marilooopess@gmail.com>
2026-09-24 13:50:28 -0300

    .

--- paginas/cadastromedico.php
+++ paginas/cadastromedico.php
@@ -22,7 +22,7 @@ $especialidades = $conexao->query("SELECT id, nome FROM especialidades ORDER BY 
     <nav>
         <ul>
             <li><a href="login.html">Login</a></li>
-            <li><a href="cadastro.html">Cadastre-se</a></li>
+            <li><a href="precadastro.html">Cadastre-se</a></li>
         </ul>
     </nav>
     <main class="container">
